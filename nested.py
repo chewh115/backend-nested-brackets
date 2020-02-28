3a502ebde21c4b93a6fa77b33544a3efdeb5a953
Chelsea <chewh115@gmail.com>
2020-02-28 10:38:31 -0500

    Updates post demo with Piero

--- nested.py
+++ nested.py
@@ -3,50 +3,45 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "chewh115, observed Chris coding with Janell assisting, and spoke with Rob about it too"
+__author__ = "chewh115, observed Chris coding w/ Janell assisting, & spoke w/ Rob about it. Also updated with demo!"
 
 import sys
+
+brackets_dict = {'(': ')', '[': ']', '{': '}', '<': '>', '(*': '*)'}
 
 
 def is_nested(line):
     """Validate a single input line for correct nesting"""
-    brackets_dict = {'(': ')', '[': ']', '{': '}', '<': '>', '(*': '*)'}
-    opening_brackets = brackets_dict.keys()
-    closing_brackets = brackets_dict.values()
     brackets_stack = []
     count = 0
     while line:
         count += 1
         if len(line) > 1:
             token = line[:2]
-            if token not in opening_brackets and token not in closing_brackets:
+            if token not in brackets_dict and token not in brackets_dict.values():
                 token = line[0]
         else:
             token = line[0]
-
-        if token in opening_brackets:
+        line = line[len(token):]
+        if token in brackets_dict:
             brackets_stack.append(token)
-        elif token in closing_brackets:
+        elif token in brackets_dict.values():
             if token == brackets_dict[brackets_stack[-1]]:
                 brackets_stack.pop()
             else:
-                return "NO " + str(count)
-        line = line[len(token):]
+                return "NO {}".format(count)
     if brackets_stack:
-        print("NO {}".format(count))
         return "NO " + str(count)
-    else:
-        print('YES')
-        return "YES"
+    return "YES"
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
-    with open(args, 'r') as input_file:
+    with open(args[0], 'r') as input_file:
         with open('output.txt', 'w') as output_file:
             for line in input_file:
                 output_file.write(is_nested(line) + '\n')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1:])
